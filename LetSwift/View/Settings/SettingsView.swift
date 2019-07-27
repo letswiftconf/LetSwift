e247ce46410ed6fca60acf6adb6590d10fb3eecb
BumMo Koo <ksquareatm@gmail.com>
2019-07-27 15:22:44 +0900

    Add settings list

--- LetSwift/View/Settings/SettingsView.swift
+++ LetSwift/View/Settings/SettingsView.swift
@@ -12,9 +12,36 @@ struct SettingsView: View {
     var body: some View {
         NavigationView {
             List {
-                Text("List")
+                Section(header: Text("General")) {
+                    Text("App Icon")
+                    Text("Permissions")
+                }
+                Section(header: Text("Conference")) {
+                    Text("About the Conference")
+                    Text("Code of Conduct")
+                    Text("Conference Feedback")
+                }
+                Section(header: Text("Links")) {
+                    Text("Let'Swift 2019")
+                    Text("Let'Swift 2018")
+                    Text("Let'Swift 2017")
+                    Text("Let'Swift 2016")
+                }
+                Section(header: Text("Developer Community")) {
+                    Text("OS X Dev.org")
+                    Text("Swift Korea Slack")
+                    Text("iOS Developers Korea Open Chat")
+                }
+                Section(header: Text("About")) {
+                    Text("Version")
+                    Text("Creators")
+                    Text("App Feedback")
+                    Text("Application Source Code")
+                    Text("Open Source Licenses")
+                }
+                .navigationBarTitle("Settings")
             }
-            .navigationBarTitle("Settings")
+            .listStyle(.grouped)
         }
     }
 }
