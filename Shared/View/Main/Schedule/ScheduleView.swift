8a5da0a6463a4749b06ef8fcb4024f06796d0fb2
delmaSong <sdy2856@gmail.com>
2020-11-22 23:40:07 +0900

    update using  model Event

--- Shared/View/Main/Schedule/ScheduleView.swift
+++ Shared/View/Main/Schedule/ScheduleView.swift
@@ -11,11 +11,12 @@ struct ScheduleView: View {
     var body: some View {
         ScrollView {
             VStack {
-                EventView(
-                    titleImageName: "dummy",
-                    title: "SwiftUI vs UIKit",
-                    description: "description",
-                    date: "2020-11-30"
+                EventView(event: Event(titleImage: "LogoLetter",
+                                       title: "SwiftUI vs UIKit",
+                                       description: "설명",
+                                       date: "11월 22일",
+                                       dayOfTheWeek: "수",
+                                       time: "19:00 - 21:00")
                 )
                     .padding([.leading, .trailing], /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
                 Text("Schedule")
