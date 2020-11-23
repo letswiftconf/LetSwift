ff774644bc17aa0e63d6eb01b10b3d62b4428d46
1Consumption <hanseop95@gmail.com>
2020-11-23 19:13:16 +0900

    Add Person Cell
    - use dummy model

--- Shared/View/Main/People/Person.swift
+++ Shared/View/Main/People/Person.swift
@@ -18,14 +18,14 @@ struct Person: Hashable, Identifiable {
 
 extension Person {
     static func makeOrganizer() -> [Person] {
-        return [Person(imageName: "chincoteague", name: "organizer", organization: nil, role: ["organizer"], description: "I'm organizer")]
+        return [Person(imageName: "sample", name: "organizer", organization: nil, role: ["organizer"], description: "I'm organizer")]
     }
     
     static func makePanels() -> [Person] {
-        return (1...4).map { return Person(imageName: "chincoteague", name: "panel\($0)", organization: "organization\($0)", role: ["panel"], description: "I'm panel\($0)") }
+        return (1...4).map { return Person(imageName: "sample", name: "panel\($0)", organization: "organization\($0)", role: ["panel"], description: "I'm panel\($0)") }
     }
     
     static func makeStaff() -> [Person] {
-        return (1...4).map { Person(imageName: "chincoteague", name: "staff\($0)", organization: "organization\($0)", role: ["staff"], description: "I'm staff\($0)") }
+        return (1...4).map { Person(imageName: "sample", name: "staff\($0)", organization: "organization\($0)", role: ["staff"], description: "I'm staff\($0)") }
     }
 }
---
+++ Shared/View/Main/People/PersonCell.swift
@@ -0,0 +1,44 @@
+//
+//  PersonCell.swift
+//  LetSwift
+//
+//  Created by 신한섭 on 2020/11/23.
+//
+
+import SwiftUI
+
+struct PersonCell: View {
+    var person: Person
+    
+    var body: some View {
+        VStack {
+            Spacer()
+            Image(person.imageName)
+                .resizable()
+                .frame(width: 80, height: 80)
+                .clipShape(Circle())
+            VStack {
+                Text(person.name)
+                    .font(.body)
+                if let organization = person.organization {
+                    Text(organization)
+                        .font(.footnote)
+                        .italic()
+                        .lineLimit(2)
+                }
+            }
+            Spacer()
+        }
+    }
+}
+
+struct PeopleCell_Previews: PreviewProvider {
+    static var previews: some View {
+        Group {
+            PersonCell(person: Person.makeOrganizer()[0])
+            PersonCell(person: Person.makePanels()[0])
+            PersonCell(person: Person.makeStaff()[0])
+        }
+        .previewLayout(.fixed(width: 300, height: 300))
+    }
+}
