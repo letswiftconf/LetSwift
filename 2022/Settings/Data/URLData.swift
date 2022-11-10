b0936191a659e48663b90c69eaa04ffb10226cb0
이가은 <rriver2@naver.com>
2022-11-10 16:54:17 +0900

    feat: setting View 추가

---
+++ 2022/Settings/Data/URLData.swift
@@ -0,0 +1,18 @@
+//
+//  URLData.swift
+//  LetSwift
+//
+//  Created by 이가은 on 2022/11/09.
+//
+
+import Foundation
+
+struct URLData {
+    static let appStoreReview = URL(string: "itms-apps://apps.apple.com/app/id1282995254?action=write-review")!
+//    static let getTicket = URL(string: "https://festa.io/events/1315")!
+//    static let askQuestion = URL(string: "https://docs.google.com/forms/d/e/1FAIpQLSfP87dYR09f3O5YVpSX5iI2IrXIroO4StEw65lUpAj3snrzQQ/viewform")!
+    static let homePage2022 = URL(string: "http://letswift.kr/2022/")!
+    static let newsletter = URL(string: "https://page.stibee.com/subscriptions/58654")!
+    static let appSourceCode = URL(string: "https://github.com/letswiftconf/LetSwift")!
+    static let webSourceCode = URL(string: "https://github.com/letswiftconf/letswift.kr")!
+}
