2ad2453e7679ddbd0560380e962749b28bacb754
이가은 <rriver2@naver.com>
2022-11-23 08:39:31 +0900

    fix: surveyData 수정

--- 2022/Playgrounds/CheeringCard/Data/SurveyData.swift
+++ 2022/Playgrounds/CheeringCard/Data/SurveyData.swift
@@ -13,14 +13,15 @@ struct SurveyData {
     var answer: [(answerId: Int, answer: String)]
 }
 // TODO: 설문, 답변 텍스트 변경 필요
+
 struct TempChartData {
     static let questionList = [
         SurveyData(
             surveyId: 1,
             question: "Let’Swift 2022 컨퍼런스에 참여하게 된 주된 이유는 무엇인가요?",
             answer: [
-                (answerId: 1, answer: "굿즈가 매력있어서"),
-                (answerId: 2, answer: "애플 워치 굿즈 받기 위해서"),
+                (answerId: 1, answer: "Swity(Let’Swift 2022 캐릭터)가 이뻐서"),
+                (answerId: 2, answer: "Let’Swift 2022 굿즈를 받기 위해서"),
                 (answerId: 3, answer: "개발 지식을 습득하기 위하여"),
                 (answerId: 4, answer: "네트워킹을 위해서")
             ]),
@@ -28,37 +29,37 @@ struct TempChartData {
             surveyId: 2,
             question: "어떤 계기로 인해 iOS 개발자로 진로를 결정하게 되셨나요?",
             answer: [
-                (answerId: 1, answer: "iOS의 ui/ux가 좋아서"),
+                (answerId: 1, answer: "iOS의 UI/UX가 좋아서"),
                 (answerId: 2, answer: "내가 아이폰을 사용하고 있기 때문에"),
-                (answerId: 3, answer: "iOS의 신기술들이 너무 좋아서"),
+                (answerId: 3, answer: "iOS의 신기술들이 좋아서"),
                 (answerId: 4, answer: "애플 생태계가 좋아서")
             ]),
         SurveyData(
             surveyId: 3,
-            question: "평소 관심있는 주제가 무엇인가요?",
+            question: "평소 관심 있는 주제가 무엇인가요?",
             answer: [
                 (answerId: 1, answer: "HIG"),
-                (answerId: 2, answer: "새 기기 (m2칩, 맥북에어, 14프로 등)"),
-                (answerId: 3, answer: "wwdc.. 신기술.. 업데이트 공부"),
-                (answerId: 4, answer: "iOS의 컨퍼런스 및 소모임")
+                (answerId: 2, answer: "애플기기"),
+                (answerId: 3, answer: "WWDC"),
+                (answerId: 4, answer: "iOS 컨퍼런스")
             ]),
         SurveyData(
             surveyId: 4,
-            question: "개발할 때 제일 싫은 부분은?",
+            question: "개발할 때 제일 싫은 상황은 언제인가요?",
             answer: [
-                (answerId: 1, answer: "디자인이 생각보다 안 이쁠 때"),
+                (answerId: 1, answer: "HIG에 맞지 않은 디자인을 받았을 때"),
                 (answerId: 2, answer: "다양한 테스트 기기가 없을 때"),
                 (answerId: 3, answer: "최소 버전이 낮아서 쓰고 싶은 기술을 사용 못할 때"),
-                (answerId: 4, answer: "말이 안통해요 ^^")
+                (answerId: 4, answer: "의사소통이 힘들 때")
             ]),
         SurveyData(
             surveyId: 5,
             question: "내 주변에 있었으면 하는 동료는?",
             answer: [
                 (answerId: 1, answer: "디자인을 눈부시게 잘하는 동료"),
-                (answerId: 2, answer: "항상 최신 기기를 가지고 있어서 장단점 다 알려주는 동료"),
+                (answerId: 2, answer: "애플 기기마다의 장단점 다 알려주는 동료"),
                 (answerId: 3, answer: "WWDC 같이 공부하는 동료"),
-                (answerId: 4, answer: "알잘딱깔센하게 알려주는 동료")
+                (answerId: 4, answer: "알잘딱깔센한 동료")
             ])
     ]
     
