16b95390b4367f8a1769425132a2566432059c9c
Noah <noa316@naver.com>
2022-11-26 04:23:46 +0900

    Feat bind to PeerConnectionController property

--- 2022/Playgrounds/CheeringCard/PeerListViewModel.swift
+++ 2022/Playgrounds/CheeringCard/PeerListViewModel.swift
@@ -5,4 +5,56 @@
 //  Created by Noah on 2022/11/26.
 //
 
+import Combine
 import Foundation
+import MultipeerConnectivity
+import NearbyInteraction
+
+final class PeerListViewModel: ObservableObject {
+    // MARK: - properties
+    
+    private var peerConnectionController: PeerConnectionController
+    @Published var isNearbySessionEstablished: Bool = false
+    @Published var distanceToPeerDevice: String = ""
+    @Published var peerName: String = ""
+    @Published var peers: [Peer] = []
+    @Published private(set) var isSupportedNearbyInteraction: Bool = false
+    @Published var isShowAlert: Bool = false
+    
+    private var cancellables = Set<AnyCancellable>()
+    
+    init(peerConnectionController: PeerConnectionController) {
+        self.peerConnectionController = peerConnectionController
+        self.bindToPeerConnectionController()
+    }
+}
+
+private extension PeerListViewModel {
+    // MARK: - private func
+    
+    func bindToPeerConnectionController() {
+        self.peerConnectionController.$connectedPeer
+            .receive(on: DispatchQueue.main)
+            .sink(receiveValue: { [weak self] peer in
+            self?.peerName = peer?.displayName ?? ""
+        }).store(in: &self.cancellables)
+        
+        self.peerConnectionController.$peers
+            .receive(on: DispatchQueue.main)
+            .sink { [weak self] peers in
+                self?.peers = peers
+            }.store(in: &self.cancellables)
+        
+        self.peerConnectionController.$isNISessionEstablished
+            .receive(on: DispatchQueue.main)
+            .sink { [weak self] isNISessionEstablished in
+                self?.isNearbySessionEstablished = isNISessionEstablished
+            }.store(in: &self.cancellables)
+        
+        self.peerConnectionController.$distanceToPeerDevice
+            .receive(on: DispatchQueue.main)
+            .sink { [weak self] distanceToPeerDevice in
+                
+            }.store(in: &self.cancellables)
+    }
+}
