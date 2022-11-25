810dfcd27b176dbe2ae2ed99308d5fcdcdb26b63
Noah <noa316@naver.com>
2022-11-26 04:26:58 +0900

    Add PeerListView file

---
+++ 2022/Playgrounds/CheeringCard/PeerListView.swift
@@ -0,0 +1,20 @@
+//
+//  PeerListView.swift
+//  LetSwift
+//
+//  Created by Noah on 2022/11/26.
+//
+
+import SwiftUI
+
+struct PeerListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PeerListView_Previews: PreviewProvider {
+    static var previews: some View {
+        PeerListView()
+    }
+}
