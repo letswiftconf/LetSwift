2eb4184aadf444b7c917340effac9350f29b35ab
hani <nellingprocess@sju.ac.kr>
2023-08-20 02:54:04 +0900

    설정 화면 톱니바퀴 탭할 때 이동할 뷰

---
+++ 2022/Settings/SettingView.swift
@@ -0,0 +1,18 @@
+//
+//  SettingView.swift
+//  LetSwift
+//
+//  Created by Hani on 2023/08/20.
+//
+
+import SwiftUI
+
+struct SettingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SettingView()
+}
