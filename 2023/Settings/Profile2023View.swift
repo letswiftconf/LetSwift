e32ca563a4516fb435078d5ef3ee0de018de2655
hani <nellingprocess@sju.ac.kr>
2023-09-20 02:12:34 +0900

    스피커 오거나이저 뷰 추가

---
+++ 2023/Settings/Profile2023View.swift
@@ -0,0 +1,21 @@
+//
+//  Profile2023View.swift
+//  LetSwift
+//
+//  Created by Hani on 2023/09/20.
+//
+
+import SwiftUI
+
+struct Profile2023View: View {
+    private let profile: Profile2023
+    
+    init(profile: Profile2023) {
+        self.profile = profile
+    }
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
