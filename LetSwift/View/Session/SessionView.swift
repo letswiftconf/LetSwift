1d2ec9fcd4711c257881996cd50bbd7814efbedf
Shawn <tomatosale7774@gmail.com>
2024-08-30 01:03:16 +0900

    Add SessionView Tab Button Event

--- LetSwift/View/Session/SessionView.swift
+++ LetSwift/View/Session/SessionView.swift
@@ -24,31 +24,50 @@ struct SessionView: View {
             case .trackB:
                 return "Track B"
             case .savedSession:
-                return "timetable.tab.savedSession"
+                return NSLocalizedString("timetable.tab.savedSession", comment: "Saved Session tab title")
             }
         }
     }
     
     var body: some View {
-        VStack {
-            HStack {
-                ForEach(Tab.allCases) { tab in
-                    ZStack {
-                        Color.darkBackground
-                        
-                        Text(tab.title)
-                            .font(.medium(size: 15))
-                            .foregroundStyle(currentTab == tab ? Color.primaryPink : Color.gray5)
+        NavigationView {
+            VStack {
+                HStack(spacing: 0) {
+                    ForEach(Tab.allCases) { tab in
+                        Button {
+                            currentTab = tab
+                        } label: {
+                            ZStack {
+                                Color.darkBackground
+                                
+                                Text(tab.title)
+                                    .font(currentTab == tab ? .medium(size: 15) : .regular(size: 15))
+                                    .foregroundStyle(currentTab == tab ? Color.primaryPink : Color.gray5)
+                            }
+                            .overlay(alignment: .bottom) {
+                                if currentTab == tab {
+                                    Color.primaryPink
+                                        .frame(height: 3)
+                                } else {
+                                    Color.gray5
+                                        .frame(height: 1)
+                                }
+                            }
+                        }
+                    }
+                }
+                .frame(height: 55)
+                
+                ScrollView {
+                    VStack {
+                        Spacer()
                     }
                 }
             }
-            .frame(height: 55)
-            
-            ScrollView {
-                VStack {
-                    Spacer()
-                }
-            }
+            .background(Color.darkBackground)
+        }
+        .tabItem {
+            Label("timetable.title", image: "ic_timetable")
         }
     }
 }
