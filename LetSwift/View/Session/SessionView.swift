c1caf03c3bfb14f54fd09616e8f6cac7e778d59b
Shawn <tomatosale7774@gmail.com>
2024-08-30 00:29:59 +0900

    Create SessionView

---
+++ LetSwift/View/Session/SessionView.swift
@@ -0,0 +1,18 @@
+//
+//  SessionView.swift
+//  LetSwift
+//
+//  Created by 박진서 on 8/30/24.
+//
+
+import SwiftUI
+
+struct SessionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SessionView()
+}
