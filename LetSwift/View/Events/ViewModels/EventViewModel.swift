3ac4d763c9f0c00e8235d2f4af42d264cb3c3d6e
Juny0110 <wnsdyds403@naver.com>
2024-09-16 18:27:38 +0900

    Add EventsView & Localizable
    - Lofi UI 적용
    - Localizable 추가

---
+++ LetSwift/View/Events/ViewModels/EventViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  EventViewModel.swift
+//  LetSwift
+//
+//  Created by 지준용 on 9/14/24.
+//
+
+import Foundation
---
+++ LetSwift/View/Events/Views/EventsView.swift
@@ -0,0 +1,131 @@
+//
+//  EventsView.swift
+//  LetSwift
+//
+//  Created by BumMo Koo on 7/13/24.
+//
+
+import SwiftUI
+
+
+struct EventsView: View {
+    
+    // MARK: - Properties
+    @State private var payload: String = ""
+    @State private var companyCellStates = Array(repeating: false, count: Company.allCases.count)
+    private var gridLayout: [GridItem] {
+        Array(repeating: GridItem(.flexible()), count: 5)
+    }
+    
+    // MARK: - Views
+    var body: some View {
+        VStack {
+            ScrollView {
+                header()
+                
+                content()
+            }
+            .padding(.horizontal, 20)
+            
+            footer()
+        }
+        .background(.darkBackground)
+        .tabItem {
+            Label("events.title", image: "ic_event")
+        }
+        .toolbarBackground(.darkBackground, for: .tabBar)
+    }
+}
+
+// MARK: - Body
+fileprivate extension EventsView {
+    @ViewBuilder
+    func header() -> some View {
+        VStack(spacing: 7) {
+            Text("스탬프를 모아보세요")
+                .foregroundStyle(.whiteText)
+                .font(.bold(size: 22))
+                .frame(maxWidth: .infinity, alignment: .leading)
+            
+            Text("""
+                 NFC 기능을 이용해 스탬프를 얻어보세요!
+                 스탬프는 이벤트 및 경품 추첨에 사용됩니다.
+                 """)
+                .foregroundStyle(.gray8)
+                .font(.regular(size: 14))
+                .frame(maxWidth: .infinity, alignment: .leading)
+        }
+        .padding(.top, 44)
+        .padding(.bottom, 23)
+    }
+    
+    @ViewBuilder
+    func content() -> some View {
+        companyEvents()
+        
+        RoundedRectangle(cornerRadius: 23, style: .continuous)
+            .foregroundStyle(Color(.systemGroupedBackground))
+            .frame(height: 200)
+    }
+    
+    @ViewBuilder
+    func footer() -> some View {
+        StampButton(payload: $payload)
+            .frame(height: 50)
+            .padding(.horizontal, 20)
+            .padding(.bottom, 14)
+    }
+}
+
+// MARK: - Content
+fileprivate extension EventsView {
+    @ViewBuilder
+    func companyEvents() -> some View {
+        LazyVGrid(columns: gridLayout, spacing: 30) {
+            ForEach(Company.allCases.indices, id: \.self) { index in
+                let company = Company.allCases[index]
+                
+                eventCell(for: company, at: index)
+            }
+        }
+        .padding(.horizontal, 18)
+        .padding(.vertical, 35)
+        .background(border())
+    }
+    
+    @ViewBuilder
+    func eventCell(for company: Company, at index: Int) -> some View {
+        RoundedRectangle(cornerRadius: 5)
+            .foregroundStyle(companyCellStates[index] ? .red : .gray8)
+            .frame(width: 50, height: 50)
+            .onChange(of: payload, { oldValue, newValue in
+                if company.payload == newValue {
+                    companyCellStates[index] = true
+                }
+            })
+    }
+    
+    func border() -> some View {
+        RoundedRectangle(cornerRadius: 10)
+            .foregroundStyle(.clear)
+            .overlay(
+                RoundedRectangle(cornerRadius: 10)
+                    .stroke(.gray)
+            )
+    }
+}
+
+
+#Preview("Event(ko)") {
+    TabView {
+        EventsView()
+            .environment(\.locale, Locale(identifier: "ko"))
+    }
+}
+
+#Preview("Event(en)") {
+    TabView {
+        EventsView()
+            .environment(\.locale, Locale(identifier: "en"))
+    }
+}
