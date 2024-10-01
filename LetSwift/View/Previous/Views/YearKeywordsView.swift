f978c27390c04acf9fb2c75ac9097c0f11d79c9b
두리 <kdle_@naver.com>
2024-10-01 17:43:52 +0900

    fix: 지난 행사 > 연도 선택 시 스크롤 이동 로직 추가

--- LetSwift/View/Previous/Views/YearKeywordsView.swift
+++ LetSwift/View/Previous/Views/YearKeywordsView.swift
@@ -8,35 +8,47 @@
 import SwiftUI
 
 struct YearKeywordsView: View {
-  @Binding var selectedYear: String
-  var years: [String]
-  
-  var body: some View {
-    ScrollView(.horizontal, showsIndicators: false) {
-      HStack(spacing: 10) {
-        ForEach(years, id: \.self) { year in
-          Text(year)
+    @Binding var selectedYear: String
+    let years: [String]
+    
+    var body: some View {
+        ScrollViewReader { proxy in
+            ScrollView(.horizontal, showsIndicators: false) {
+                HStack(spacing: 10) {
+                    ForEach(years, id: \.self) { year in
+                        items(year)
+                    }
+                }
+                .padding(.horizontal, 10)
+            }
+            .padding(.leading, 5)
+            .padding(.bottom, 18)
+            .onChange(of: selectedYear) { oldValue, newValue in
+                withAnimation {
+                    proxy.scrollTo(newValue, anchor: .center)
+                }
+            }
+        }
+    }
+    
+    @ViewBuilder
+    func items(_ year: String) -> some View {
+        Text(year)
             .padding(.horizontal, 13)
             .padding(.vertical, 6)
             .background(selectedYear == year ? .primaryPink.opacity(0.2) : .gray2)
             .overlay(
-              RoundedRectangle(cornerRadius: 30)
-                .stroke(
-                  selectedYear == year ? .primaryPink : .clear,
-                  lineWidth: 1
-                )
+                RoundedRectangle(cornerRadius: 30)
+                    .stroke(
+                        selectedYear == year ? .primaryPink : .clear,
+                        lineWidth: 1
+                    )
             )
             .foregroundStyle(selectedYear == year ? .primaryPink : .gray9)
             .font(selectedYear == year ? .semiBold(size: 13) : .regular(size: 13))
             .cornerRadius(30)
             .onTapGesture {
-              selectedYear = year
+                selectedYear = year
             }
-        }
-      }
-      .padding(.horizontal, 10)
     }
-    .padding(.leading, 5)
-    .padding(.bottom, 18)
-  }
 }
