e552bf7375281b7ef1d76a94e6b90d02193de3a8
Inhwan Kim <ih.kim@vinulabs.com>
2025-09-30 11:31:24 +0900

    Refactor: extract initializeApp function.

--- LetSwift/AppDelegate.swift
+++ LetSwift/AppDelegate.swift
@@ -15,25 +15,29 @@ class AppDelegate: NSObject, UIApplicationDelegate {
         NotificationManager.shared.setDelegate()
         return true
     }
-    
+
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        initializeApp(application)
+        return true
+    }
+
+    private func initializeApp(_ application: UIApplication) {
         FirebaseApp.configure()
-        
+
         // 앱 실행 시 사용자에게 알림 허용 권한을 받음
         UNUserNotificationCenter.current().delegate = self
-        
+
         let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound] // 필요한 알림 권한을 설정
         UNUserNotificationCenter.current().requestAuthorization(
             options: authOptions,
             completionHandler: { _, _ in }
         )
-        
+
         // UNUserNotificationCenterDelegate를 구현한 메서드를 실행시킴
         application.registerForRemoteNotifications()
-        
+
         // 파이어베이스 Meesaging 설정
         Messaging.messaging().delegate = self
-        return true
     }
 }
 
@@ -41,7 +45,7 @@ extension AppDelegate: UNUserNotificationCenterDelegate {
     func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
         Messaging.messaging().apnsToken = deviceToken
     }
-    
+
     func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification) async -> UNNotificationPresentationOptions {
         return [.list, .banner, .sound]
     }
