c6415e6da090c2a61e35be350398a6c40f839e39
Bummo Koo <ksquareatm@gmail.com>
2025-10-12 12:22:44 +0900

    Replace custom search view with native search.

--- LetSwift/Views/Previous/Views/SearchView.swift
+++ LetSwift/Views/Previous/Views/SearchView.swift
@@ -1,42 +1,42 @@
+////
+////  SearchView.swift
+////  LetSwift
+////
+////  Created by Hyun A Song on 9/26/24.
+////
 //
-//  SearchView.swift
-//  LetSwift
+//import SwiftUI
 //
-//  Created by Hyun A Song on 9/26/24.
+//// MARK: - Search View
+//struct SearchView: View {
+//  @Binding var searchText: String
+//  
+//  var body: some View {
+//    HStack {
+//      Image(systemName: "magnifyingglass")
+//        .foregroundStyle(.gray5)
+//      
+//      TextField(
+//        text: $searchText,
+//        label: {
+//          Text("세션 이름을 검색해보세요")
+//            .foregroundStyle(.gray5)
+//            .font(.system(size: 14))
+//        }
+//      )
+//      .foregroundStyle(.white)
+//    }
+//    .padding(.horizontal, 18)
+//    .padding(.vertical, 7)
+//    .overlay(
+//      RoundedRectangle(cornerRadius: 30)
+//        .stroke(.gray5, lineWidth: 1)
+//    )
+//    .padding(.horizontal, 20)
+//    .padding(.vertical, 18)
+//  }
+//}
 //
-
-import SwiftUI
-
-// MARK: - Search View
-struct SearchView: View {
-  @Binding var searchText: String
-  
-  var body: some View {
-    HStack {
-      Image(systemName: "magnifyingglass")
-        .foregroundStyle(.gray5)
-      
-      TextField(
-        text: $searchText,
-        label: {
-          Text("세션 이름을 검색해보세요")
-            .foregroundStyle(.gray5)
-            .font(.system(size: 14))
-        }
-      )
-      .foregroundStyle(.white)
-    }
-    .padding(.horizontal, 18)
-    .padding(.vertical, 7)
-    .overlay(
-      RoundedRectangle(cornerRadius: 30)
-        .stroke(.gray5, lineWidth: 1)
-    )
-    .padding(.horizontal, 20)
-    .padding(.vertical, 18)
-  }
-}
-
-#Preview {
-    PreviousView()
-}
+//#Preview {
+//    PreviousView()
+//}
