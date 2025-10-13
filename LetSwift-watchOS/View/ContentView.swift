3f6f4c14a26df638435bcd4ed073b9b3c990472b
Bummo Koo <ksquareatm@gmail.com>
2025-10-13 09:43:35 +0900

    Add watchOS target.

---
+++ LetSwift-watchOS/View/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LetSwift-watchOS Watch App
+//
+//  Created by BumMo Koo on 10/13/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, Let'Swift!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
