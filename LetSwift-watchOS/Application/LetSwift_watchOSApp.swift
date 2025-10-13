bddcafdcbeae73de982795e337abd59ade2a6169
Bummo Koo <ksquareatm@gmail.com>
2025-10-13 09:43:35 +0900

    Add watchOS target.

---
+++ LetSwift-watchOS/Application/LetSwift_watchOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  LetSwift_watchOSApp.swift
+//  LetSwift-watchOS Watch App
+//
+//  Created by BumMo Koo on 10/13/25.
+//
+
+import SwiftUI
+
+@main
+struct LetSwift_watchOS_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
